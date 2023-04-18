baf499576e245aa38e60e9e1282731d416986926
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-04-18 18:50:49 +0700

    CRUD with ReactJS - displaying user data to view (READ)

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,81 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
 class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
 
-    state = {
-
+  async componentDidMount() {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
     }
+  }
+  /*
+life cycle
+1. Run contruct -> init state
+2. Did mount (set state)
+3. render
+*/
+  render() {
+    console.log("chekc render", this.state);
+    let arrUsers = this.state.arrUsers;
+    return (
+      <div className="users-container">
+        <div className="title text-center">Manage users with taihn</div>
+        <div className="users-table mt-3 mx-1">
+          <h1>A Fancy Table</h1>
 
-    componentDidMount() {
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>First name</th>
+              <th>Last Name</th>
+              <th>Address</th>
+              <th>Actions</th>
+            </tr>
 
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+            {arrUsers &&
+              arrUsers.map((item, index) => {
+                console.log("taihn check map", item, index);
+                return (
+                  <tr>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button className="btn-edit">
+                        <i className="fas fa-edit"></i>
+                      </button>
+                      <button className="btn-delete">
+                        <i className="fas fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,5 +3,7 @@ import axios from "../axios";
 const hanldeLoginApi = (userEmail, userPassword) => {
   return axios.post("/api/login", { email: userEmail, password: userPassword });
 };
-
-export { hanldeLoginApi };
+const getAllUsers = (inputId) => {
+  return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+export { hanldeLoginApi, getAllUsers };
