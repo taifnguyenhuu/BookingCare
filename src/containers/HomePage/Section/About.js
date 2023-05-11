f97a9ba7497f4cc9da0f18e2f8f00a7837e0f338
Nguyen Huu Tai <nguyenhuutaidev@gmail.com>
2023-05-11 09:32:02 +0700

    Updat section about header

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -6,14 +6,16 @@ class About extends Component {
   render() {
     return (
       <div className="section-share section-about">
-        <div className="section-about-header">Người truyền cảm hứng</div>
+        <div className="section-about-header">
+          Truyền thông nói về BookingCare
+        </div>
         <div className="section-about-content">
           <div className="content-left">
             <iframe
               width="100%"
               height="400px"
-              src="https://www.youtube.com/embed/Llw9Q6akRo4?list=PLE_DkwW5bt2F-BJ-tmof9GzMbS2aQ5EP7"
-              title="LẠC TRÔI | OFFICIAL MUSIC VIDEO | SƠN TÙNG M-TP"
+              src="https://www.youtube.com/embed/ZHZnfwoqsHs"
+              title="VITV - Startup 360 - Bác sĩ và bệnh nhân nói về BookingCare"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowfullscreen
@@ -21,13 +23,17 @@ class About extends Component {
           </div>
           <div className="content-right">
             <p>
-              Casio PX-S1000 là một cây đàn đơn giản dể sử dụng nhờ vào bảng
-              điều khiển cảm ứng. Chạm vào bảng điều khiển phí trên các phím đàn
-              và các điều khiển sẽ xuất hiện. Khi không sử dụng, bảng điều khiển
-              sẽ tắt đi giúp bạn tập trung vào việc biểu diễn mà không bị phân
-              tâm. Tất cả những chức năng của cây đàn đều nằm ngay trước mặt
-              bạn, bao gồm cả tùy chọn âm sắc mà không cần truy cập vào bất cứ
-              menu nào.
+              BookingCare là nền tảng đặt lịch khám bệnh đầu tiên ở nước ta, kết
+              nối với trên 80 bệnh viện, phòng khám tại Hà Nội và Tp.HCM.
+              BookingCare tập trung vào đặt lịch khám nhóm bệnh chuyên khoa,
+              bệnh mạn tính, những người biết rõ tình trạng của mình, chủ động
+              sắp xếp kế hoạch đi khám và bệnh không có tính chất cấp cứu, Sau
+              hơn 2 năm chính thức ra mắt, đến nay BookingCare đã phục vụ hơn
+              40.000 nghìn lượt người đi khám thành công tại các bệnh viện,
+              phòng khám. BookingCare đang hợp tác triển khai đặt khám theo yêu
+              cầu tại một số bệnh viện lớn tại Hà Nội như Bệnh viện Việt Đức,
+              Bệnh viện Thanh Nhàn, Bệnh viện Da liễu Hà Nội, Bệnh viện Châm cứu
+              Trung ương, Bệnh viện Đa khoa Bảo Sơn, Bệnh viện Quốc tế Thu Cúc…
             </p>
           </div>
         </div>
